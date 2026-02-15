10d1db9c4d1f480f23b49cb2f6408678e9ba2f18
fajrijuanda <if22.fajrijuanda@mhs.ubpkarawang.ac.id>
2026-02-15 16:53:47 +0700

    Fix build.sh python environment (unset PYTHONHOME) and remove redundant collectstatic

--- apps/api/build.sh
+++ apps/api/build.sh
@@ -2,6 +2,12 @@
 # exit on error
 set -o errexit
 
-# Dependencies are installed by buildpack, skipping pip install
-python manage.py collectstatic --no-input
+# Fix for potential python path issues in some environments
+unset PYTHONHOME
+unset PYTHONPATH
+
+# Dependencies are installed by buildpack, skip pip install
+# collectstatic is handled by buildpack
+# python manage.py collectstatic --no-input
+
 python manage.py migrate
