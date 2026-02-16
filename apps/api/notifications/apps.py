0598f5ea72cabb56cf53b846e773726279532dad
fajrijuanda <if22.fajrijuanda@mhs.ubpkarawang.ac.id>
2026-02-16 09:33:20 +0700

    feat: Add notifications app configuration and establish main API URL routing.

--- apps/api/notifications/apps.py
+++ apps/api/notifications/apps.py
@@ -4,5 +4,5 @@ class NotificationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'notifications'
 
-    def ready(self):
-        import notifications.signals
+    # def ready(self):
+    #     import notifications.signals
